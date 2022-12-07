b57f55b44a68a80ec156a131ef95584370731aa7
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2022-12-06 23:50:49 -0500

    create operator dashboard view

--- frontmkaly/pages/dashboard.js
+++ frontmkaly/pages/dashboard.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
-import {Grid} from '@mui/material';
+import {Box, Container, Grid, Stack} from '@mui/material';
 import BlogCard from '../src/components/dashboard/BlogCard';
 import SalesOverview from '../src/components/dashboard/SalesOverview';
 import DailyActivity from '../src/components/dashboard/DailyActivity';
 import ProductPerfomance from '../src/components/dashboard/ProductPerfomance';
+import BaseCard from '../src/components/baseCard/BaseCard';
+import {OptionButton} from '../src/components';
 
 const Dashboard = () => {
-	const [userRol, setUserRol] = useState('client');
+	const [userRol, setUserRol] = useState('operator');
 
 	if (userRol === 'client') {
 		return <ClientDashboard />;
@@ -14,6 +16,10 @@ const Dashboard = () => {
 
 	if (userRol === 'admin') {
 		return <AdminDashboard />;
+	}
+
+	if (userRol === 'operator') {
+		return <OperatorDashboard />;
 	}
 };
 
@@ -71,21 +77,29 @@ const ManagerDashboard = () => {
 const OperatorDashboard = () => {
 	return (
 		<>
-			<Grid container spacing={0}>
-				<Grid item xs={12} lg={12}>
-					<SalesOverview />
-				</Grid>
-				{/* ------------------------- row 1 ------------------------- */}
-				<Grid item xs={12} lg={4}>
-					<DailyActivity />
-				</Grid>
-				<Grid item xs={12} lg={8}>
-					<ProductPerfomance />
-				</Grid>
-				<Grid item xs={12} lg={12}>
-					<BlogCard />
-				</Grid>
-			</Grid>
+			{/* Container */}
+			<Box
+				sx={{
+					// backgroundColor: 'red',
+					width: '100%',
+					height: '100%',
+					display: 'flex',
+					justifyContent: 'center',
+				}}
+			>
+				<BaseCard title="GESTIÓN CLIENTE">
+					<Stack direction="row" spacing={10}>
+						<OptionButton title="REGISTRAR CLIENTE" icon="client" />
+						<OptionButton title="MODIFICAR CLIENTE" icon="edit" />
+					</Stack>
+					<br />
+					<br />
+					<Stack direction="row" spacing={10}>
+						<OptionButton title="CONSULTAR CLIENTE" icon="search" />
+						<OptionButton title="REGISTRAR PAGO" icon="payment" />
+					</Stack>
+				</BaseCard>
+			</Box>
 		</>
 	);
 };
---
+++ frontmkaly/styles/globals.js
@@ -0,0 +1,7 @@
+const colors = {
+	primary: '#00408F',
+	secondary: '#FDC500',
+	blue_200: '#3C73AA',
+};
+
+export {colors};
