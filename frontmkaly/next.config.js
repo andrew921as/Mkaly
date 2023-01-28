513f71b788f73dae728d49d1f3f61b8aaa54ba69
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2023-01-28 15:18:46 -0500

    hotfix next.config.js

--- frontmkaly/next.config.js
+++ frontmkaly/next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  i18n:{
-    locales: ['en', 'es'],
-    defaultLocale: 'en'
-  }
+	reactStrictMode: true,
+	i18n: {
+		locales: ['en', 'es'],
+		defaultLocale: 'en',
+	},
 	images: {
 		domains: ['assets.example.com', 'res.cloudinary.com'],
 	},
 };
-
 
 module.exports = nextConfig;
